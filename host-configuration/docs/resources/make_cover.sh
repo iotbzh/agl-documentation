686663b0678f7e365b15abac46ed24fb2ee1f5a4
Sebastien Douheret <sebastien.douheret@iot.bzh>
2018-05-18 16:59:12 +0200

    Fixed pdf cover generation.
    Use gendocs.sh script to auto-generate pdf cover.
    Also add latest improvements (eg. warning tags support)
    
    Signed-off-by: Sebastien Douheret <sebastien.douheret@iot.bzh>

--- host-configuration/docs/resources/make_cover.sh
+++ host-configuration/docs/resources/make_cover.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
+DOCS_DIR=$(cd $(dirname $0)/.. && pwd)
+BOOKFILE=$DOCS_DIR/../book.json
 
-TITLE="AGL Development Kit"
-SUBTITLE="Developer Documentation"
-VERSION="Version 3.1"
-DATE="March 2017"
+TITLE=$(grep  '"title":' $BOOKFILE | cut -d'"' -f 4)
+SUBTITLE=$(grep  '"subtitle":' $BOOKFILE | cut -d'"' -f 4)
+VERSION="Version $(grep  '"version":' $BOOKFILE | cut -d'"' -f 4)"
+DATE=$(grep  '"published":' $BOOKFILE | cut -d'"' -f 4)
 
-cat cover.svg | sed -e "s/{title}/$TITLE/g" \
-    -e "s/font-size:87.5px/font-size:50px/g" \
+[ -z "$TITLE" ] && { echo "Error TITLE not set!" ; exit 1; }
+[ -z "$VERSION" ] && { echo "Error VERSION not set!" ; exit 1; }
+[ -z "$DATE" ] && { echo "Error DATE not set!" ; exit 1; }
+
+
+cat $(dirname $0)/cover.svg | sed -e "s/{title}/$TITLE/g" \
+    -e "s/font-size:87.5px/font-size:54px/g" \
     -e "s/{subtitle}/$SUBTITLE/g" \
-    -e "s/font-size:62.5px/font-size:45px/g" \
+    -e "s/font-size:62.5px/font-size:40px/g" \
     -e "s/{version}/$VERSION/g" \
     -e "s/{date}/$DATE/g" \
     > /tmp/cover.svg
 
 # use  imagemagick convert tool  (cover size must be 1800x2360)
 convert -resize "1600x2160!" -border 100 -bordercolor white -background white \
-    -flatten -quality 100 /tmp/cover.svg ../cover.jpg
-[ "$?" = "1" ] && { echo "Error while generating cover.jpg"; exit 1; }
+    -flatten -quality 100 /tmp/cover.svg $DOCS_DIR/cover.jpg
 
-convert -resize "200x262!" ../cover.jpg ../cover_small.jpg
-[ "$?" = "1" ] && { echo "Error while generating cover_small.jpg"; exit 1; }
+convert -resize "200x262!" $DOCS_DIR/cover.jpg $DOCS_DIR/cover_small.jpg
